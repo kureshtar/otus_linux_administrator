d462844d769a40df459fca2302f228b03c1d1344
kureshtar <52793935+kureshtar@users.noreply.github.com>
2023-10-09 15:29:52 +0300

    Update mail_send.sh

--- HomeWork10_bash/mail_send.sh
+++ HomeWork10_bash/mail_send.sh
@@ -2,6 +2,19 @@
 DATE=$(date -d '1 hour ago' "+%d/%m/%Y %H:00:00")
 ACCESS_LOG='/var/log/httpd/access_log'
 ERROR_LOG='/var/log/httpd/error_log'
+
+RUN_FLAG=/tmp/watchdog.run
+function finish {
+    rm -f "$RUN_FLAG";
+    exit 0;
+}
+
+if [[ -f $RUN_FLAG ]]; # если создан флаг, скрипт уже запущен или некорректно завершён
+    then echo "!!! SCRIPT ALWAYS RUNNING OR UNSUCCESSFULY COMPLITED !!! If you're sure, delete the file /tmp/watchdog.run and run again."; exit 1;
+fi
+touch $RUN_FLAG # создание флага запущенного скрипта
+trap finish EXIT # при нормальном завершении, при получении прерывающего сигнала, который может быть обработан удалить флаг запуска
+
 #echo "Статистика за последний час начиная с $DATE"
 COUNT_IP=$(grep "`date +"%d"`.*`date -d '1 hour ago' +"%H"`:[0-9][0-9]:[0-9][0-9]" $ACCESS_LOG | awk {'print $1'} | sort | uniq -c)
 COUNT_URL=$(grep "`date +"%d"`.*`date -d '1 hour ago' +"%H"`:[0-9][0-9]:[0-9][0-9]" $ACCESS_LOG | awk {'print $7'} | sort | uniq -c)
@@ -10,3 +23,5 @@ COUNT_STATUS=$(grep "`date +"%d"`.*`date -d '1 hour ago' +"%H"`:[0-9][0-9]:[0-9]
 LIST_ERROR=$(grep "`date +"%d"`.*`date -d '1 hour ago' +"%H"`:[0-9][0-9]:[0-9][0-9]" $ERROR_LOG)
 
 echo -e "Статистика за последний час начиная с $DATE\n\n$COUNT_IP\n\n$COUNT_URL\n\n$COUNT_STATUS\n\n\nОшибки за последний час начиная с $DATE\n\n$LIST_ERROR" | mail -s "New report" -r kentonik@yandex.ru
+
+finish # очистка флага запущенного скрипта и выход
